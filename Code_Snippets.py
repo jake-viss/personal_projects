2086e5668fd5d1d5b8763b6ac0dce9f45b6ec3da
Jacob_Vissering <jake.vissering@gmail.com>
2021-08-30 18:41:16 -0700

    Starting a personal projects file

---
+++ Code_Snippets.py
@@ -0,0 +1,16 @@
+# This will contain code & ideas for ways I can use python in my personal life.
+
+def average(aList):
+    total = 0
+    count = 0
+    for number in aList:
+        total =+ number
+        count =+ 1
+    calculated_average = total/count
+    return calculated_average
+
+list =[1,2,3,4,5]
+
+list_average = average(list)
+print(list_average)
+    
