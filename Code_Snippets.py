2087547f32baceb158557a6df439d9cb51f6c6a7
Jacob_Vissering <jake.vissering@gmail.com>
2021-09-07 20:18:49 -0700

    update average function

--- Code_Snippets.py
+++ Code_Snippets.py
@@ -1,16 +1,12 @@
 # This will contain code & ideas for ways I can use python in my personal life.
 
-def average(aList):
-    total = 0
+def calculate_mean(data_set):
+    sum = 0
     count = 0
-    for number in aList:
-        total =+ number
-        count =+ 1
-    calculated_average = total/count
-    return calculated_average
+    for value in data_set:
+        sum = value + sum
+        count = count + 1
+    return sum / count
 
-list =[1,2,3,4,5]
 
-list_average = average(list)
-print(list_average)
     
